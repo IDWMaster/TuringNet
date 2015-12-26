c8986da45b24401c5d3e38442741c942d0e1bdbb
Brian Bosak <webadm@elcnet.servehttp.com>
2015-12-26 14:38:20 -0600

    Added Thresholding neural network

--- trainprim.js
+++ trainprim.js
@@ -25,25 +25,41 @@ var table = [
     
 ];
 basenet.train(table,{errorThresh:.05,iterations:200000,log:true,logPeriod:20,learningRate:.2});
-//Test the neural-net
-//AND
-console.log(basenet.run([0,0,0]) > .5);
-console.log(basenet.run([0,0,1]) > .5);
-console.log(basenet.run([0,1,1]) > .5);
-console.log(basenet.run([0,1,0]) > .5);
+var test = function(net,trainingData) {
+    for(var i = 0;i<trainingData.length;i++) {
+        if(!((net.run(trainingData[i].input)[0] > .5) == (trainingData[i].output[0]> .5))) {
+            
+            return false;
+        }
+    }
+    return true;
+};
+//Make a neural-network
+var mknet = function(trainingData) {
+    var net = new brain.NeuralNetwork({hiddenLayers:[80]});
+    net.train(trainingData,{errorThresh:.05,iterations:200000,log:true,logPeriod:20,learningRate:.2});
+    test(net,trainingData);
+    return net;
+};
 
-//OR
-console.log(basenet.run([.4,0,0]) > .5);
-console.log(basenet.run([.4,0,1]) > .5);
-console.log(basenet.run([.4,1,1]) > .5);
-console.log(basenet.run([.4,1,0]) > .5);
-//NOT
-console.log(basenet.run([.9,0,0]) > .5);
-console.log(basenet.run([.9,0,1]) > .5);
-console.log(basenet.run([.9,1,1]) > .5);
-console.log(basenet.run([.9,1,0]) > .5);
+if(!test(basenet,table)) {
+    throw 'Training of basenet failed.';
+}
+
+var nets = {};
+nets.logic = basenet.toJSON();
+
+
+//Create thresholding neural-network
+table = new Array();
+var accuracy = 500;
+for(var i = 0;i<accuracy;i++) {
+    table.push({input:[i/accuracy],output:[((i/accuracy)>.5)*1]});
+}
+nets.threshold = mknet(table);
+
 
 //Output the primitive neural-network to the training file
 var output = fs.createWriteStream('binops.json');
-output.write(JSON.stringify(basenet.toJSON()));
+output.write(JSON.stringify(nets));
 output.end();
